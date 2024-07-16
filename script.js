3625455f543865f0e34fc0d153c0d2e340c32772
Shakir Ullah <shakirullahaup@gmail.com>
2024-07-16 17:36:21 +0500

    chap 9 done

---
+++ script.js
@@ -0,0 +1,51 @@
+// chap no 9 ( prompts)
+
+prompt("thank you sir");
+prompt("how are you sir");
+prompt("nice to meet you sir");
+prompt("hi gyues");
+prompt("great fulleness");
+prompt("my name is shakir ullah");
+prompt("my father name is said nabi khan");
+prompt("i am pakistani");
+prompt("where are you froom");
+prompt("where are you in swabi");
+prompt("which class do you read");
+prompt("how many you are siblig");
+prompt("thank you sir");
+
+const userAge=prompt("enter your age");
+console.log(you are ,+userAge,+"year old");
+
+
+const userName=prompt("please enter your username");
+alert("helloooo"+userName);
+console.log("hellooo"+userName)
+
+const userId=prompt("enter your id");
+alert("done"+userI);
+
+const firstName=prompt("enter your first name");
+const lastName=prompt("enter your last name");
+const fullName=firstName+lastName;
+alert("hellooo"+" "+fullName);
+
+const firstAge=prompt("enter your first age");
+const lastAge=prompt("enter your last age");
+const fullAge=firstAge+lastAge;
+alert("shakir "+" "+fullAge);
+
+// this case total bill is for exemplple 2+2=22 not 4
+
+const pencil=prompt("price for pencill");
+const noteBook=prompt("price for notebook");
+const totalBill=pencil+noteBook;
+alert("your total bills is"+" "+totalBill);
+
+// this case before the prompt is + sigen is shows is combine for example 2+2=4 not 22 because + sign use bsfore the prompt
+
+
+const pencil=+prompt("price for pencill");
+const noteBook=+prompt("price for noteBook");
+const totalBill=pencil+noteBook;
+alert("your total bills is"+totalBill);
